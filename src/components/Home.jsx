4dde64fb742b0aa5b5ec1915e47ae396d02d91bd
wabbi-khan <kwahab789@gmail.com>
2023-03-01 17:36:53 +0500

    ok

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,36 +6,36 @@ import Header from './Header';
 import { HiSpeakerWave, HiSpeakerXMark } from 'react-icons/hi2';
 import CopyRight from './CopyRight';
 // import audio1 from '../audioPlay/1.wav';
-import audio1 from '../audioPlay/hello.m4a';
+import audio1 from '../audioPlay/ambience_loop.mp3';
 const Home = () => {
   // ======audio=========
-  const [playing, setPlaying] = useState(false);
-  const player = new Audio(audio1);
-  useEffect(() => {
-    playing ? player.play() : player.pause();
-    return () => player.pause();
-  }, [playing]);
+  // const [playing, setPlaying] = useState(false);
+  // const player = new Audio(audio1);
+  // useEffect(() => {
+  //   playing ? player.play() : player.pause();
+  //   return () => player.pause();
+  // }, [playing]);
 
-  function togglePlay() {
-    setPlaying((s) => !s);
-  }
+  // function togglePlay() {
+  //   setPlaying((s) => !s);
+  // }
   // ====== it is working on firefox audio play automatic when ever page reload
-  // const [isPlaying, setIsPlaying] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
 
-  // useEffect(() => {
-  //   const audio = document.getElementById('myAudio');
+  useEffect(() => {
+    const audio = document.getElementById('myAudio');
 
-  //   if (isPlaying) {
-  //     audio.pause();
-  //   } else {
-  //     audio.play();
-  //   }
-  // }, [isPlaying]);
+    if (isPlaying) {
+      audio.pause();
+    } else {
+      audio.play();
+    }
+  }, [isPlaying]);
 
-  // useEffect(() => {
-  //   const audio = document.getElementById('myAudio');
-  //   audio.play();
-  // }, []);
+  useEffect(() => {
+    const audio = document.getElementById('myAudio');
+    audio.play();
+  }, []);
   // ======================
 
   // ==========================
@@ -74,8 +74,8 @@ const Home = () => {
       </div>
       <div className="features">
         <div className="soundIcon">
-          {/* <div>
-            <audio id="myAudio" autoPlay={true} src={audio1} /> 
+          <div>
+            <audio id="myAudio" autoPlay={true} loop={true} src={audio1} />
             <div onClick={() => setIsPlaying(!isPlaying)}>
               {isPlaying ? (
                 <HiSpeakerXMark style={{ cursor: 'pointer' }} />
@@ -83,15 +83,14 @@ const Home = () => {
                 <HiSpeakerWave style={{ cursor: 'pointer' }} />
               )}
             </div>
-            
-          </div> */}
-          <div onClick={() => togglePlay()}>
+          </div>
+          {/* <div onClick={() => togglePlay()}>
             {playing ? (
               <HiSpeakerWave style={{ cursor: 'pointer' }} />
             ) : (
               <HiSpeakerXMark style={{ cursor: 'pointer' }} />
             )}
-          </div>
+          </div> */}
         </div>
         <div>
           <h2>
