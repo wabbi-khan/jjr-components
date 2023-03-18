faf4b06cf76f3e18f787e6c6ae1156aa8a143ea7
wabbi-khan <kwahab789@gmail.com>
2023-03-18 15:28:04 +0500

    add xcl web link

--- src/components/CopyRight.jsx
+++ src/components/CopyRight.jsx
@@ -1,10 +1,16 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
 import React from 'react';
 
 const CopyRight = ({ additionalClass }) => {
   return (
     <div className={additionalClass}>
       © JJR NETWORK 2023 - All Right Reserved <br />
-      <span>Powered by XCL TECHNOLOGIES</span>
+      <span>
+        Powered by{' '}
+        <a href="https://xcltechnologies.com/" target={'blank'}>
+          Xcl Technologies
+        </a>
+      </span>
     </div>
   );
 };
